9e0f1d36ba2e9437f2533a834aa0303c36f72bdf
user <james@gmail.com>
2020-03-11 18:11:30 +0100

    1 commit consola

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 	
 	String estado = "genial!";
+	String estado1 = "guay!";
+	String estado2 = "no tan bien";
+	
 	
 	System.out.println("Me encuentro " + estado);
 		
