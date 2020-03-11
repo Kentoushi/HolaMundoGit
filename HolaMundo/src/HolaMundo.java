46537bfaf9c9b912be64eb22404f64f5421f507b
user <james@gmail.com>
2020-03-11 18:06:08 +0100

    1 commit Eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,8 +4,10 @@ public class HolaMundo {
 	public static void main(String[] args) {
 	
 	String estado = "genial!";
+	String estado2 = "mas o menos!";
 	
 	System.out.println("Me encuentro " + estado);
+	System.out.println("Me encuentro " + estado2);
 		
 	}
 
