ebda8657e93c5e6af2250c3d619ea69aca7e0241
Kentoushi <jpd.sealey@gmail.com>
2020-03-11 19:38:21 +0100

    Update HolaMundoGit.java

--- HolaMundoGit/src/HolaMundoGit.java
+++ HolaMundoGit/src/HolaMundoGit.java
@@ -4,9 +4,11 @@ public class HolaMundoGit {
 	public static void main(String[] args) {
 		String estado = "muy bien";
 		String estado2 = "no tan bien";
+		String estado3 = "genial!!";
 		
 		System.out.println("Estoy " + estado);
 		System.out.println("Estoy " + estado2);
+		System.out.println("Estoy " + estado3);
 
 	}
 
